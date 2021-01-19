e5a57732c65e2bdbe563bea964651d332c61dc51
21slee <72581436+21slee@users.noreply.github.com>
2021-01-19 15:14:55 -0800

    Update field.py

--- field.py
+++ field.py
@@ -1,20 +1,31 @@
 import numpy as np
-
+import random
 
 class Field:
 
     def __init__(self):
         self.field = np.zeros(1, dtype=int)
+        self.row_width = 12
+        self.platform_width = 3
+        self.platform_needed = True
         pass  # placeholder
 
     def update(self):  # main method/function
+        if self.platform_needed:
+            generate_platform(self)
+            self.platform_needed = False
         pass
 
     def generate_coin(self):
         pass
 
     def generate_platform(self):
-        pass
+        platform = np.rows(self.row_width)
+        platform_start = random.randrange(0, self.row_width-self.platform_width)
+        for i in range(platform_start, platform_start + self.platform_width):
+            platform[i] = 3
+        
+        return platform
 
     def copy(self):
         return self.field.copy()

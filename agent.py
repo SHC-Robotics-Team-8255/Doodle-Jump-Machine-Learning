44e798caaec679e5aa8173e3bdd7a5df070236f1
Pascal Reich <44716533+22preich@users.noreply.github.com>
2021-02-27 20:47:48 -0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,5 +2,5 @@ from game import Game
 import os
 
 os.mkdir(os.path.join(os.getcwd(), "artifacts"))
-with open("artifacts/artifact.txt") as file:
+with open("artifacts/artifact.txt", "w") as file:
     file.write("test123")
